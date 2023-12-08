374ba0e6cd6031fa904f69c64c909638864855b8
s78910 <57589877+s78910@users.noreply.github.com>
2023-12-08 14:01:10 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,16 +4,12 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=KernelSU by SnowWolf725 @ xda-developers
+kernel.string=KernelSU by QQ1342985312 @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=OnePlus7
-device.name2=OnePlus7Pro
-device.name3=OnePlus7T
-device.name4=OnePlus7TPro
 supported.versions=
 supported.patchlevels=
 '; } # end properties
